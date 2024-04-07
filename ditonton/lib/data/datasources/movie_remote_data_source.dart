c1ea78ee867a790c345c95b6aa3847cbc8d0f36b
Fortissible <wildanfajri85@gmail.com>
2024-04-07 23:26:54 +0700

    Create tv response, model and entity, also set up remote data source for tv api

--- ditonton/lib/data/datasources/movie_remote_data_source.dart
+++ ditonton/lib/data/datasources/movie_remote_data_source.dart
@@ -4,7 +4,10 @@ import 'package:ditonton/data/models/movie_detail_model.dart';
 import 'package:ditonton/data/models/movie_model.dart';
 import 'package:ditonton/data/models/movie_response.dart';
 import 'package:ditonton/common/exception.dart';
+import 'package:ditonton/data/models/tv_model.dart';
 import 'package:http/http.dart' as http;
+
+import '../models/tv_response.dart';
 
 abstract class MovieRemoteDataSource {
   Future<List<MovieModel>> getNowPlayingMovies();
@@ -13,11 +16,19 @@ abstract class MovieRemoteDataSource {
   Future<MovieDetailResponse> getMovieDetail(int id);
   Future<List<MovieModel>> getMovieRecommendations(int id);
   Future<List<MovieModel>> searchMovies(String query);
+
+  Future<List<TvModel>> getNowPlayingTvSeries();
+  Future<List<TvModel>> getTopRatedTvSeries();
+  Future<List<TvModel>> getPopularTvSeries();
 }
 
 class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
   static const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
   static const BASE_URL = 'https://api.themoviedb.org/3';
+
+  static const ENDPOINT_URL_TV_TOPRATED = "/tv/top_rated";
+  static const ENDPOINT_URL_TV_POPULAR = "/tv/popular";
+  static const ENDPOINT_URL_TV_ONTHEAIR= "/tv/on_the_air";
 
   final http.Client client;
 
@@ -90,6 +101,42 @@ class MovieRemoteDataSourceImpl implements MovieRemoteDataSource {
 
     if (response.statusCode == 200) {
       return MovieResponse.fromJson(json.decode(response.body)).movieList;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> getNowPlayingTvSeries() async {
+    final response =
+        await client.get(Uri.parse('$BASE_URL$ENDPOINT_URL_TV_ONTHEAIR?$API_KEY'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).results;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> getPopularTvSeries() async {
+    final response =
+        await client.get(Uri.parse('$BASE_URL$ENDPOINT_URL_TV_POPULAR?$API_KEY'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).results;
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<TvModel>> getTopRatedTvSeries() async {
+    final response =
+        await client.get(Uri.parse('$BASE_URL$ENDPOINT_URL_TV_TOPRATED?$API_KEY'));
+
+    if (response.statusCode == 200) {
+      return TvResponse.fromJson(json.decode(response.body)).results;
     } else {
       throw ServerException();
     }
---
+++ ditonton/lib/data/models/tv_response.dart
@@ -0,0 +1,43 @@
+// To parse this JSON data, do
+//
+//     final tvResponse = tvResponseFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:ditonton/data/models/tv_model.dart';
+import 'package:equatable/equatable.dart';
+
+TvResponse tvResponseFromJson(String str) => TvResponse.fromJson(json.decode(str));
+
+String tvResponseToJson(TvResponse data) => json.encode(data.toJson());
+
+class TvResponse extends Equatable{
+  final int page;
+  final List<TvModel> results;
+  final int totalPages;
+  final int totalResults;
+
+  TvResponse({
+    required this.page,
+    required this.results,
+    required this.totalPages,
+    required this.totalResults,
+  });
+
+  factory TvResponse.fromJson(Map<String, dynamic> json) => TvResponse(
+    page: json["page"],
+    results: List<TvModel>.from(json["results"].map((x) => TvModel.fromJson(x))),
+    totalPages: json["total_pages"],
+    totalResults: json["total_results"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "page": page,
+    "results": List<dynamic>.from(results.map((x) => x.toJson())),
+    "total_pages": totalPages,
+    "total_results": totalResults,
+  };
+
+  @override
+  List<Object?> get props => [page, results, totalPages, totalResults];
+}
